439b8258723125e8f59b8ddce79b2243426a9047
양희연 <huiyeony888@gmail.com>
2024-07-27 00:06:55 +0900

    페이지 라우팅

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import { Routes ,Route } from 'react-router-dom'
 import Home from './pages/Home'
 import New from './pages/New'
 function App() {
-  return <div>
-    <div>홈 페이지</div>
+  return (<div>
+    
     <Routes>
       <Route path='/' element= {<Home/>} />
       <Route path='/new' element= {<New/>} />
     </Routes>
   </div>
+  )
 }
 
 export default App
